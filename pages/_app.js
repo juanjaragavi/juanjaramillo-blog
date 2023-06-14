2e12d528a6a2a0d9b513b20d8992a64508b30467
TRADEBOG <juan.miguel.jaramillo1984@gmail.com>
2023-06-14 13:33:26 -0500

    Analytics conectado (falta testing)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,28 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 import 'prismjs/themes/prism-tomorrow.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-13WZEVPR65"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-13WZEVPR65');
+        `,
+        }}
+      />
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
